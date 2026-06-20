35291f4990e3b0336cc965c34b091772148bf17d
Bishal Sharma <bishal.subbedi@gmail.com>
2026-06-20 17:10:47 +0545

    error handled ppp

--- todolist/todoapp/urls.py
+++ todolist/todoapp/urls.py
@@ -13,7 +13,6 @@ urlpatterns = [
     path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
     path('complete/<int:task_id>/', views.complete_task, name='complete_task'),
     path('edit/<int:task_id>/', views.edit_task, name="edit_task"),
-    path('clear_all/', views.clear_all_tasks, name='clear_all_tasks'),
-    
+    path('clear_all_tasks/', views.clear_all_tasks, name='clear_all_tasks'),    
 
 ]
